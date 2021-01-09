45cc844bf83fa63c1e9215b473f5684a3afa11cd
Stan-Green <Stan@MComputerSolutions.com>
2021-01-09 18:00:34 -0500

    Update uptime.cpp

--- uptime.cpp
+++ uptime.cpp
@@ -12,11 +12,11 @@
 int main()
 {
     ULONGLONG ll_milSec = GetTickCount64();
-    long YEAR = 31536000000;
-    long DAY = 86400000;
-    long HOUR = 3600000;
-    long MINUTE = 60000;
-    long SECOND = 1000;
+    ULONGLONG YEAR = 31536000000;
+    LONG DAY = 86400000;
+    LONG HOUR = 3600000;
+    LONG MINUTE = 60000;
+    LONG SECOND = 1000;
 
     std::string ls_years = "0";
     std::string ls_days = "0";
@@ -26,11 +26,11 @@ int main()
     std::string ls_milseconds = "0";
 
 
-    UINT li_years = 0;
-    UINT li_days = 0;
-    UINT li_hours = 0;
-    UINT li_minutes = 0;
-    UINT li_seconds = 0;
+    LONG ll_years = 0;
+    LONG ll_days = 0;
+    LONG ll_hours = 0;
+    LONG ll_minutes = 0;
+    LONG ll_seconds = 0;
 
     time_t now = time(0);
     tm localTime;
@@ -42,39 +42,39 @@ int main()
     std::string ls_milSec = std::to_string(ll_milSec);
     std::cout << "Uptime in milliseconds: " << ls_milSec << "\n\n";
 
-    li_years = ll_milSec / YEAR;
-    if (li_years > 0)
+    ll_years = ll_milSec / YEAR;
+    if (ll_years > 0)
     {
-        ll_milSec = ll_milSec - (li_years * YEAR);
-        ls_years = std::to_string(li_years);
+        ll_milSec = ll_milSec - (ll_years * YEAR);
+        ls_years = std::to_string(ll_years);
     }
 
-    li_days = ll_milSec / DAY;
-    if (li_days > 0)
+    ll_days = ll_milSec / DAY;
+    if (ll_days > 0)
     {
-        ll_milSec = ll_milSec - (li_days * DAY);
-        ls_days = std::to_string(li_days);
+        ll_milSec = ll_milSec - (ll_days * DAY);
+        ls_days = std::to_string(ll_days);
     }
 
-    li_hours = ll_milSec / HOUR;
-    if (li_hours > 0)
+    ll_hours = ll_milSec / HOUR;
+    if (ll_hours > 0)
     {
-        ll_milSec = ll_milSec - (li_hours * HOUR);
-        ls_hours = std::to_string(li_hours);
+        ll_milSec = ll_milSec - (ll_hours * HOUR);
+        ls_hours = std::to_string(ll_hours);
     }
  
-    li_minutes = ll_milSec / MINUTE;
-    if (li_minutes > 0)
+    ll_minutes = ll_milSec / MINUTE;
+    if (ll_minutes > 0)
     {
-        ll_milSec = ll_milSec - (li_minutes * MINUTE);
-        ls_minutes = std::to_string(li_minutes);
+        ll_milSec = ll_milSec - (ll_minutes * MINUTE);
+        ls_minutes = std::to_string(ll_minutes);
     }
 
-    li_seconds = ll_milSec / SECOND;
-    if (li_seconds > 0)
+    ll_seconds = ll_milSec / SECOND;
+    if (ll_seconds > 0)
     {
-        ll_milSec = ll_milSec - (li_seconds * SECOND);
-        ls_seconds = std::to_string(li_seconds);
+        ll_milSec = ll_milSec - (ll_seconds * SECOND);
+        ls_seconds = std::to_string(ll_seconds);
     }
     
     if (ll_milSec > 0)
